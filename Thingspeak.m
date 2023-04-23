2238c0125cbd634d4b3a47c90e60b6610988311d
peppevenezia <90832856+peppevenezia@users.noreply.github.com>
2023-04-23 16:07:05 +0200

    Delete Thingspeak.m

--- Thingspeak.m
+++
@@ -1,6 +0,0 @@
-thingSpeakURL = 'http://api.thingspeak.com/';
-thingSpeakWriteURL = [thingSpeakURL 'update'];
-writeApiKey = 'VU9QO1Y7269MEORN';
-fieldName = 'Data';
-fieldValue = 4;
-response = webwrite(thingSpeakWriteURL,'api_key',writeApiKey,fieldName,fieldValue)
